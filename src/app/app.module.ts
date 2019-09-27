82a17de68fca9edf4d945448418f58daa055d500
soaresmju <ju.soares.moraes@outlook.com>
2019-09-27 17:39:44 -0300

    inserindo modulo somar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SomarModule } from './somar/somar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SomarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
