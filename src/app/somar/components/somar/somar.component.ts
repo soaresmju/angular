798c9d5d5423e9a706f3e67ecf9bf669ce7b6c5c
soaresmju <ju.soares.moraes@outlook.com>
2019-10-18 17:15:37 -0300

    conta

--- src/app/somar/components/somar/somar.component.ts
+++ src/app/somar/components/somar/somar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SomarService } from '../../service';
+import { NumberFormatStyle } from '@angular/common';
 
 @Component({
   selector: 'app-somar',
@@ -10,17 +11,43 @@ export class SomarComponent implements OnInit {
 
   constructor(private somarService: SomarService) { }
   private valor: number;
+  private lanche: number;
+  private bebida: number;
+  private doce: number;
 
   ngOnInit() {
-  this.valor = 0;
+    this.valor = 0;
+    this.lanche = 0;
+    this.bebida = 0;
+    this.doce = 0;
   }
 
-    calcula(valorRecebido:number){
+    calcula(valorRecebido:number, visorMostra:number){
       this.valor = this.somarService.somar(this.valor, valorRecebido);
+      if(visorMostra==1){
+        this.lanche = this.lanche + 1;
+      }
+      if(visorMostra==2){
+        this.bebida = this.bebida + 1;
+      }
+      if(visorMostra==3){
+        this.doce = this.doce + 1;
+      }
     }
 
     get pegaValor(){
       return this.valor;
     }
 
+    get visorLanche(){
+      return this.lanche;
+    }
+
+    get visorBebida(){
+      return this.bebida;
+    }
+
+    get visorDoce(){
+      return this.doce;
+    }
 }
